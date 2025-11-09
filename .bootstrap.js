576716507992872f2eb3dd03846b299a8a673928
Vibesdk <vibesdk-bot@cloudflare.com>
2025-11-09 05:40:57 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add mysql2@^3.10.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
